fec47664fe524e14a77aa6332b32fa8d1636290e
Tugba <tubaayalcin@gmail.com>
2022-03-13 00:26:59 +0300

    day12Pojo

--- src/test/java/get_http_request/day12/PostRequestPojo01.java
+++ src/test/java/get_http_request/day12/PostRequestPojo01.java
@@ -1,8 +1,16 @@
 package get_http_request.day12;
 
+import base_url.JsonPlaceHolderBaseUrl;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.Assert;
 import org.junit.Test;
+import pojos.JsonPlaceHolderPojo;
 
-public class PostRequestPojo01
+import static io.restassured.RestAssured.expect;
+import static io.restassured.RestAssured.given;
+
+public class PostRequestPojo01 extends JsonPlaceHolderBaseUrl
 {
     /*
   https://jsonplaceholder.typicode.com/todos url 'ine bir request gönderildiğinde
@@ -24,6 +32,29 @@ Status kodun 201, response body 'nin ise
     @Test
     public void test01()
     {
+        // 1. URL OLUSTUR
+        spec04.pathParams("first","todos");
+
+        // 2.EXPECTED DATA OLUSTUR
+        JsonPlaceHolderPojo expectedData = new JsonPlaceHolderPojo(21,201,"Tidy your room",false);
+        System.out.println("expectedData = " + expectedData);
+
+        // 3. REQUEST VE RESPONSE
+        Response response = given().contentType(ContentType.JSON).spec(spec04)
+                .body(expectedData).when().post("/{first}");
+
+        response.prettyPrint();
+
+        // 4. DOGRULAMA
+        // 1. De-Serialization
+        JsonPlaceHolderPojo actualData = response.as(JsonPlaceHolderPojo.class);
+        Assert.assertEquals(201,response.getStatusCode());
+        Assert.assertEquals(expectedData.getId(),actualData.getId());
+        Assert.assertEquals(expectedData.getUserId(),actualData.getUserId());
+        Assert.assertEquals(expectedData.getTitle(),actualData.getTitle());
+        Assert.assertEquals(expectedData.isCompleted(),actualData.isCompleted());
+
+
 
     }
 }
